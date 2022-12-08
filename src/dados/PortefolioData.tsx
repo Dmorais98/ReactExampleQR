85b70a98dacca156bfbff1e755d927510ddba802
RPMT1996 <ruipedroteixeira1996@hotmail.com>
2022-12-08 15:34:07 +0000

    Portofolio
    Portefólio bonito

---
+++ src/dados/PortefolioData.tsx
@@ -0,0 +1,30 @@
+import poap1 from '../assets/bored.jpg';
+import poap2 from '../assets/chefpizza.jpg';
+import poap3 from '../assets/monkey.jpg';
+import poap4 from '../assets/sad.jpg';
+import poap5 from '../assets/xray.jpg';
+
+const PortefolioData = [
+    {
+        imgsrc: poap1,
+        title: "You met Sam in December 2022!"
+    },
+    {
+        imgsrc: poap2,
+        title: "Congrats! You made it through the semester!"
+    },
+    {
+        imgsrc: poap3,
+        title: "Gnosis Ethereum Meetup - Christmas Edition"
+    },
+    {
+        imgsrc: poap4,
+        title: "December Test"
+    },
+    {
+        imgsrc: poap5,
+        title: "NFT Stammtisch - 06.12.2022"
+    }
+]
+
+export default PortefolioData;
